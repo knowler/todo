13376a4736d3088a5acba4403c4e4b090fd38518
Nathan Knowler <nathan@knowler.me>
2018-12-27 00:09:41 -0800

    Create todo storage at local data path

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+extern crate dirs;
+
 use std::fs::DirBuilder;
 use std::path::Path;
 
@@ -5,10 +7,18 @@ pub struct App;
 
 impl App {
     pub fn new() {
-        let path = "todos";
+        match dirs::data_local_dir() {
+            Some(path) => {
+                let data = path.display().to_string() + "/knowler.todos";
 
-        if !Path::new(path).exists() {
-            DirBuilder::new().recursive(true).create(path).unwrap();
+                if Path::new(&data).exists() {
+                    println!("Todos storage exists at {}", &data);
+                } else {
+                    DirBuilder::new().recursive(true).create(&data).unwrap();
+                    println!("Created todos storage at {}", &data);
+                }
+            },
+            None => println!("Can't find local data directory!"),
         }
     }
 }
